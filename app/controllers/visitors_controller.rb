0b45892485effa1842a60790596e09f34e374f3a
Josh Wykell <jwykell@gmail.com>
2015-08-22 13:14:47 -0400

    latest speaker apears on home

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,5 @@
 class VisitorsController < ApplicationController
+  def index
+    @current_speakers=Speaker.where("date >= ?", Date.today).limit(1)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root to: 'visitors#index'
-  devise_for :users
 end
